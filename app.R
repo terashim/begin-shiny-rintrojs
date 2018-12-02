75ddc751b241d6b2ac2fdedac46087a90c33aae5
Terashi, Masaki <terashi.masaki@gmail.com>
2018-12-02 20:17:53 +0900

    rintrojsを使ってチュートリアルを追加

--- app.R
+++ app.R
@@ -1,31 +1,39 @@
 library(shiny)
+library(rintrojs) # パッケージを読み込む
 
 # アプリケーションのUIを定義
 ui <- fluidPage(
   
+  introjsUI(), # これをUI定義のどこかに含める
+  
   # アプリケーションのタイトル
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("rintrojsで手軽にチュートリアルを実装"),
   
   # サイドバー
   sidebarLayout(
     sidebarPanel(
-      # スライダー
-      sliderInput("bins",
-                  "ビンの数:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      introBox( # チュートリアルのステップ
+        sliderInput("bins", "ビンの数:", min = 1, max = 50, value = 30), # ハイライトされる要素
+        data.step = 1, # ステップ番号
+        data.intro = "このスライダーを動かしてビンの数を変更してください" # このステップのメッセージ
+      ),
+      
+      actionButton("tutorial", "チュートリアルを開始")
     ),
     
     # メインパネル
     mainPanel(
-      plotOutput("distPlot")
+      introBox( # チュートリアルのステップ
+        plotOutput("distPlot"),
+        data.step = 2, # ステップ番号
+        data.intro = "スライダーの値に合わせてこのヒストグラムが変化します" # このステップのメッセージ
+      )
     )
   )
 )
 
 # サーバー側ロジックを定義
-server <- function(input, output) {
+server <- function(input, output, session) {
   
   output$distPlot <- renderPlot({
     # ui.Rから来た input$bins に基づいてビンを生成
@@ -35,6 +43,21 @@ server <- function(input, output) {
     # 指定のビン数でヒストグラムを描画
     hist(x, breaks = bins, col = 'darkgray', border = 'white')
   })
+  
+  observeEvent(
+    input$tutorial, # tutorialボタンがクリックされたら
+    introjs( # チュートリアルを開始する
+      session,
+      options = list( # オプション設定
+        nextLabel = "次へ", # 次のステップへ進むボタンのラベル
+        prevLabel = "戻る", # 前のステップへ戻るボタンのラベル
+        skipLabel = "やめる", # チュートリアルをスキップするボタンのラベル
+        doneLabel = "完了", # チュートリアルを完了するボタンのラベル
+        showBullets = FALSE, # 進捗を表す中点マークを表示する/しない
+        showProgress = TRUE # プログレスバーを表示する/しない
+      )
+    )
+  )
 }
 
 # アプリケーションを起動する
