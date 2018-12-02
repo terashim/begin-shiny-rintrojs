499212968ff48fee25c89599cc0dfefd73b43d77
Terashi, Masaki <terashi.masaki@gmail.com>
2018-12-02 20:08:54 +0900

    コメントを日本語に変更

--- app.R
+++ app.R
@@ -1,48 +1,40 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+# UI
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  
+  # アプリケーションのタイトル
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # サイドバー
+  sidebarLayout(
+    sidebarPanel(
+      # スライダー
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # メインパネル
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
 )
 
-# Define server logic required to draw a histogram
+# サーバー側
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+  output$distPlot <- renderPlot({
+    # ui.Rから来た input$bins に基づいてビンを生成
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # 指定のビン数でヒストグラムを描画
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
 }
 
 # Run the application 
